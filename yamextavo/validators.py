e84ac469af958e30e2fe4ca208114607c3839c89
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-12-27 07:57:15 +0100

    #8 - Removed type "YamaleRequiredReferenceFacade" from the "validators.py" file

--- yamextavo/validators.py
+++ yamextavo/validators.py
@@ -1,24 +1,7 @@
 import threading
 from yamale.validators import Validator, Any
-import itertools
-import yamale
-from yamale import validators
-
 from yamextavo.utils import YamlFileHelper
 
-
-class YamaleRequiredReferenceFacade():
-
-    def __init__(self, schema_file):
-        validators_list = validators.DefaultValidators
-        validators_list[RequiredReferenceValidator.__name__] = RequiredReferenceValidator
-        validators_list[RequiredReferenceValidator.tag] = RequiredReferenceValidator
-        self.yamale_schema = yamale.make_schema(schema_file, validators=validators_list)
-
-    def validate(self, data_file_path):
-        yamale_data = itertools.chain(*map(yamale.make_data, [data_file_path]))
-        threading.current_thread().__setattr__("yaml_data_store", YamlFileHelper(data_file_path))
-        return yamale.validate(self.yamale_schema, yamale_data) is not None
 
 class RequiredReferenceValidator(Any):
     tag = 'ref_req'

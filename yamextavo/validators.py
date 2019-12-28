d06d7e698d9c22272ad9fb5058f59da0dad8bbcf
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-12-28 20:16:15 +0100

    #13 - Added "TODO" comment related to code refactor [skip travis]

--- yamextavo/validators.py
+++ yamextavo/validators.py
@@ -22,9 +22,9 @@ class RequiredReferenceValidator(Any):
         if not hasattr(ct, 'yaml_data_store'):  #TODO Move to extractYamlDataStore method
             pass #TODO return Validation error  #TODO Move to extractYamlDataStore method
         yaml_data_store = threading.current_thread().__getattribute__("yaml_data_store")    #TODO Move to extractYamlDataStore method
-        if yaml_data_store is not None and  isinstance(yaml_data_store, YamlFileHelper) :
-            if yaml_data_store.contains_jsonpath(self.jsonpath):
-                yaml_document = yaml_data_store.return_first_value_by_jsonpath(self.jsonpath)
-                if yaml_document is not None and yaml_document.strip() != "":
+        if yaml_data_store is not None and  isinstance(yaml_data_store, YamlFileHelper) :   #TODO Move to extractYamlDataStore method
+            if yaml_data_store.contains_jsonpath(self.jsonpath):    #TODO Move to retrieveJsonNode method
+                yaml_document = yaml_data_store.return_first_value_by_jsonpath(self.jsonpath)   #TODO Move to retrieveJsonNode method
+                if yaml_document is not None and yaml_document.strip() != "":   #TODO  Move to checkIfPropertyIsRequired method
                     required = True
         return not required

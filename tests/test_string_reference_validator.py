c052a5a07453f35857c609e71d2eef4f85af0d46
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-01-06 17:00:29 +0100

    #15 - Added test for invalid Yaml file [skip travis]

--- tests/test_string_reference_validator.py
+++ tests/test_string_reference_validator.py
@@ -48,5 +48,25 @@ class TestRequiredReferenceValidator(unittest.TestCase):
             # then
             pass
 
+    def test_should_return_validation_error_for_invalid_schema_where_child_element_is_required_but_not_exists(self):
+        # given
+        tested = YamaleRequiredReferenceFacade(self.schema_file)
+        test_file = path.join(os.path.dirname(__file__), "string_reference_validator/schemas/1/invalid_content_required_message/non_empty_primary_node.yml")
+        with open(test_file, "r") as f:
+            yaml_data = yaml.full_load(f)
+            self.assertFalse("dependent_node2" in yaml_data, "Yaml should not contains property \"dependent_node2\"")
+            self.assertTrue("primary_node2" in yaml_data, "Yaml should contains property \"primary_node2\"")
+            self.assertEqual("parent", yaml_data.get("primary_node2"), "The element \"primary_node2\" should have value \"parent\"")
+
+        try:
+            # when
+            tested.validate(test_file)
+            self.fail("Expected ValueError")
+
+        except ValueError:
+            # then
+            pass
+
+
 if __name__ == '__main__':
     unittest.main()

e93ab0984ea1b951c90e9db457bd5f63c82e0844
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-01-06 16:49:30 +0100

    #15 - Added test for invalid Yaml file [skip travis]

--- tests/test_string_reference_validator.py
+++ tests/test_string_reference_validator.py
@@ -17,7 +17,7 @@ class TestRequiredReferenceValidator(unittest.TestCase):
             yaml_data = yaml.full_load(f)
             self.assertTrue("dependent_node2" in yaml_data, "Yaml should contains property \"dependent_node2\"")
             self.assertEqual("xxx", yaml_data.get("dependent_node2"), "The element \"dependent_node2\" should have value \"xxx\"")
-            self.assertFalse("dependent_node2" in yaml_data, "Yaml should not contains property \"dependent_node2\"")
+            self.assertFalse("primary_node2" in yaml_data, "Yaml should not contains property \"dependent_node2\"")
 
         try:
             # when
@@ -28,6 +28,25 @@ class TestRequiredReferenceValidator(unittest.TestCase):
             # then
             pass
 
+    def test_should_return_validation_error_for_invalid_schema_where_child_element_has_invalid_content_and_primary_node_is_empty(self):
+        # given
+        tested = YamaleRequiredReferenceFacade(self.schema_file)
+        test_file = path.join(os.path.dirname(__file__), "string_reference_validator/schemas/1/invalid_content_validation_message/empty_parent_invalid_child.yml")
+        with open(test_file, "r") as f:
+            yaml_data = yaml.full_load(f)
+            self.assertTrue("dependent_node2" in yaml_data, "Yaml should contains property \"dependent_node2\"")
+            self.assertEqual("xxx", yaml_data.get("dependent_node2"), "The element \"dependent_node2\" should have value \"xxx\"")
+            self.assertTrue("primary_node2" in yaml_data, "Yaml should contains property \"primary_node2\"")
+            self.assertEqual(None, yaml_data.get("primary_node2"), "The element \"primary_node2\" should have value \"None\"")
+
+        try:
+            # when
+            tested.validate(test_file)
+            self.fail("Expected ValueError")
+
+        except ValueError:
+            # then
+            pass
 
 if __name__ == '__main__':
     unittest.main()
